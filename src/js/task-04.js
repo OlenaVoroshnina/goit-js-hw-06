94e12e50745730b34fb5fec87ee696825655050d
Olena Voroshnina <yuriyilena67@gmail.com>
2022-12-20 13:35:08 +0100

    added script to task-04

---
+++ src/js/task-04.js
@@ -0,0 +1,32 @@
+// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue.
+
+const counterValue = {
+    value: 0,
+    increment() {
+        this.value += 1;
+    },
+
+    decrement() {
+        this.value -= 1;
+    },
+};
+
+
+
+const decrementBtn = document.querySelector('.js-decrement');
+console.log(decrementBtn);
+
+const incrementBtn = document.querySelector('.js-increment');
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', function () {
+    counterValue.decrement();
+    valueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener('click', function () {
+    counterValue.increment();
+    valueEl.textContent = counterValue.value;
+});
