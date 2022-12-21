bccee1b1caadd31ed8798f9bb9e4843b7593faa9
Olena Voroshnina <yuriyilena67@gmail.com>
2022-12-21 23:08:41 +0100

    added script to task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,4 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const buttonEl = document.querySelector('.login-form > button');
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick (event) {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+};
